3bc8b2106c8f2063f051f57b8dc19c2d2f19c8e5
Paul DAO <dao.van.paul@gmail.com>
2021-08-07 23:24:53 +0200

    FEAT component logo

---
+++ src/components/logo/logo.component.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './logo.style.scss';
+
+const LogoComponent = () => {
+    return (
+        <div className='logo'>
+            <div className="logo__icon"></div>
+            <div className="logo__name">Fasterize</div>
+        </div>   
+    )
+} 
+
+export default LogoComponent;
