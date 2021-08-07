62dbe51cbdd1aa1ea8da06b9d3ba1e1fcabf568d
Paul DAO <dao.van.paul@gmail.com>
2021-08-07 23:32:13 +0200

    FEAT DebuggerPage : simulate response url plug

--- src/pages/fasterize-debugger/DebuggerPage.tsx
+++ src/pages/fasterize-debugger/DebuggerPage.tsx
@@ -3,12 +3,13 @@ import SideBarComponent from '../../components/side-bar/side-bar.component';
 import TopBarComponent from '../../components/top-bar/top-bar.component';
 import SectionTitleBar from '../../components/section-title-bar/section-title-bar.component';
 import './debugger-page.style.scss'
-
+import ResultPlug from '../../shared/_models/reponse-plug.model';
+import UrlPLugService from '../../shared/serices/urlPlug.service';
 
 const DebuggerPage = () => {
-
-    const [url, setUrl] = useState<String>('');
-
+    const urlPlug = new UrlPLugService();
+    const [url, setUrl] = useState<string>('');
+    const [resultsPlug, setResultsPlug] = useState<ResultPlug[]>([]);
 
     const handlerUrlChange = (url: string) => {
         setUrl(url);
@@ -16,7 +17,12 @@ const DebuggerPage = () => {
     const handlerSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();  
         console.log(url);
-
+        urlPlug.getResultUrlPLug(url).then((res: ResultPlug) => {
+            console.log(res);
+            setResultsPlug(prevResult => [res, ...prevResult]);
+            console.log(resultsPlug);
+            
+        })
     }
 
     return (
@@ -39,7 +45,18 @@ const DebuggerPage = () => {
                     <section>
                         <SectionTitleBar title='HISTORY'/>
                         {/* Table */}
-
+                        {
+                            resultsPlug.length === 0 ? 
+                            '' 
+                            : 
+                            resultsPlug.map( (plug, i) => 
+                                <div className="row" key={i}>
+                                    <p>
+                                    {plug.plugged ? 'true': 'false'}
+                                    </p>
+                                </div>
+                            )
+                        }
                     </section>
                 </div>
             </main>
