5b9f7da1248a8474939492ad78333c9d50f344e1
Paul DAO <dao.van.paul@gmail.com>
2021-08-08 20:40:33 +0200

    FEAT service url plug : fetch api data

--- src/shared/serices/urlPlug.service.ts
+++ src/shared/serices/urlPlug.service.ts
@@ -1,15 +1,12 @@
 import ResponsePlug from '../_models/reponse-plug.model';
+
 export default class UrlPLugService {
     
     public async getResultUrlPLug(url: string): Promise<ResponsePlug> {
-        const inputs: ResponsePlug =  {
-            plugged: true,
-            statusCode: 200,
-            fstrzFlags: ['optimisée', 'cachée'],
-            cloudfrontStatus: 'MISS',
-            cloudfrontPOP: 'Frankfurt'
-        }
-        const result = new ResponsePlug(inputs);
-        return result
+        const urlEncoded = encodeURIComponent(url);
+        return await fetch(`http://localhost:3004/${urlEncoded}`).then( async (res) => {
+            const data = await res.json();
+            return new ResponsePlug(data);
+        });
     }
 }
