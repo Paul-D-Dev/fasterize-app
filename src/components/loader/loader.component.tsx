b76f2c73029ae0213362def344e70ca5b64de082
Paul DAO <dao.van.paul@gmail.com>
2021-08-09 22:02:02 +0200

    FEAT Component Loader

---
+++ src/components/loader/loader.component.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './loader.style.scss';
+
+const LoaderComponent = () => {
+    return (
+        <div className="loader"></div>
+    )
+}
+
+export default LoaderComponent;
