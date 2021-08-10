32717d5afc8596c08c1e68fcbf9fb6c78c4b4c3a
Paul DAO <dao.van.paul@gmail.com>
2021-08-10 22:16:44 +0200

    URL API set in env variable

--- src/shared/serices/urlPlug.service.ts
+++ src/shared/serices/urlPlug.service.ts
@@ -1,10 +1,11 @@
 import ResponsePlug from '../_models/reponse-plug.model';
 
 export default class UrlPLugService {
+    private readonly $URL_API = process.env.NODE_ENV ==='development' ? process.env.REACT_APP_DEV_API : process.env.REACT_APP_PROD_API;
     
     public async getResultUrlPLug(url: string): Promise<ResponsePlug> {
         const urlEncoded = encodeURIComponent(url);
-        return await fetch(`http://localhost:3004/${urlEncoded}`).then( async (res) => {
+        return await fetch(`${this.$URL_API}${urlEncoded}`).then( async (res) => {
             const data = await res.json();
             return new ResponsePlug(data);
         });
