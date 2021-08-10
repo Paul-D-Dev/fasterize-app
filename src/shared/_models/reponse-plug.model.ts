ab8ca6f0858bd98295f865583d1bc51105665c7c
Paul DAO <dao.van.paul@gmail.com>
2021-08-10 21:14:45 +0200

    UPDATE class response plug api : optional variable

--- src/shared/_models/reponse-plug.model.ts
+++ src/shared/_models/reponse-plug.model.ts
@@ -2,9 +2,9 @@ import { IResponsePlug } from './../_interfaces/response-plug.interface';
 export default class ResponsePlug implements IResponsePlug {
     plugged!: boolean;
     statusCode!: number;
-    fstrzFlags!: string[];
-    cloudfrontStatus!: string;
-    cloudfrontPOP!: string;
+    fstrzFlags?: string[];
+    cloudfrontStatus?: string;
+    cloudfrontPOP?: string;
     url?: string;
     date?: string;
 
