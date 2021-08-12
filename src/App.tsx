d3fcc3cb723b77d3e8188a1ba7df437f86148b61
Paul DAO <dao.van.paul@gmail.com>
2021-08-12 13:43:10 +0200

    CHORES APP clear useless

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,13 @@
 import React from 'react';
-import { BrowserRouter as Router, Link, Route} from 'react-router-dom';
-import DebuggerPage from './pages/fasterize-debugger/DebuggerPage';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.scss';
+import DebuggerPage from './pages/fasterize-debugger/DebuggerPage';
 
 function App() {
   return (
     <Router>
 
     <div className="App">
-      <header className="App-header">
-
-      </header>
       <Route path='/' component={DebuggerPage}></Route>
     </div>
 
